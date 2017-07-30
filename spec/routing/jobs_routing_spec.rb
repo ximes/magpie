1dc7943359d93db01f020a42a8db8a5d1bb550bd
Stefano Franzin <stefano.franzin@gmail.com>
2017-07-30 21:33:40 +0200

    Basic jobs scaffold

--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe JobsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/jobs").to route_to("jobs#index")
+      expect(get: "/jobs").to route_to("jobs#index")
     end
 
     it "routes to #new" do
-      expect(:get => "/jobs/new").to route_to("jobs#new")
+      expect(get: "/jobs/new").to route_to("jobs#new")
     end
 
     it "routes to #show" do
-      expect(:get => "/jobs/1").to route_to("jobs#show", :id => "1")
+      expect(get: "/jobs/1").to route_to("jobs#show", id: "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/jobs/1/edit").to route_to("jobs#edit", :id => "1")
+      expect(get: "/jobs/1/edit").to route_to("jobs#edit", id: "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/jobs").to route_to("jobs#create")
+      expect(post: "/jobs").to route_to("jobs#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/jobs/1").to route_to("jobs#update", :id => "1")
+      expect(put: "/jobs/1").to route_to("jobs#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/jobs/1").to route_to("jobs#update", :id => "1")
+      expect(patch: "/jobs/1").to route_to("jobs#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/jobs/1").to route_to("jobs#destroy", :id => "1")
+      expect(delete: "/jobs/1").to route_to("jobs#destroy", id: "1")
     end
 
   end
