c2fbd038d169e8b22538dc26cf575ce88163609c
Stefano Franzin <stefano.franzin@gmail.com>
2017-08-06 14:58:20 +0200

    custom devise controllers location

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
-    registrations: "registrations",
-    sessions: "sessions",
+    registrations: "users/registrations",
   }
 
   resources :jobs
