7874ac0c9f0b7066bfe5e9e11dba5e655b82a2a8
Stefano Franzin <stefano.franzin@gmail.com>
2017-08-06 15:39:38 +0200

    toggleing jobs confirmed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,20 @@
 //= require cocoon
 //= require gentelella
 //= require gentelella-custom
+
+function toggle_job_customization(){
+  var conf_container = $("#custom-configuration");
+  if ($("#job_customizable:checked").length == 0){
+    conf_container.slideUp(); 
+      conf_container.find("input:hidden, .configuration-field").prop("disabled", true);
+  } else {
+    conf_container.slideDown();
+    conf_container.find("input:hidden, .configuration-field").prop("disabled", false);
+  }
+}
+
+
+$(document).ready(function(){
+  $("#job_customizable").on('click', toggle_job_customization);
+  toggle_job_customization();
+});
