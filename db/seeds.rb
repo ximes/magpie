25a7fe9c08abe1db7fef0f275ff286fdb77226c9
Stefano Franzin <stefano.franzin@gmail.com>
2017-08-18 17:57:01 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: "admim@example.com", password: "password")
 
 Atom.create(name: "Click", class_name: "Atoms::Events::Click", enabled: true)
-Atom.create(name: "CssSelector", class_name: "Atoms::Matchers::Click", enabled: true)
-Atom.create(name: "Failed", class_name: "Atoms::Markers::Click", enabled: true)
-Atom.create(name: "Successful", class_name: "Atoms::Markers::Click", enabled: true)
-Atom.create(name: "Refresh", class_name: "Atoms::PageActions::Click", enabled: true)
-Atom.create(name: "Text", class_name: "Atoms::Values::Click", enabled: true)
-Atom.create(name: "Value", class_name: "Atoms::Values::Click", enabled: true)
+Atom.create(name: "CssSelector", class_name: "Atoms::Matchers::CssSelector", enabled: true)
+Atom.create(name: "Failed", class_name: "Atoms::Markers::Failed", enabled: true)
+Atom.create(name: "Successful", class_name: "Atoms::Markers::Successful", enabled: true)
+Atom.create(name: "Refresh", class_name: "Atoms::PageActions::Refresh", enabled: true)
+Atom.create(name: "Text", class_name: "Atoms::Values::Text", enabled: true)
+Atom.create(name: "Value", class_name: "Atoms::Values::Value", enabled: true)
