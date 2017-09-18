628c037a8329689d4bb686df4d884fa33b1c29f8
Stefano Franzin <stefano.franzin@gmail.com>
2017-09-18 20:15:35 +0200

    Sidekiq/scheduler adjustments

--- app/workers/scheduler_worker.rb
+++ app/workers/scheduler_worker.rb
@@ -3,17 +3,9 @@
 class SchedulerWorker
   include Sidekiq::Worker
   def perform(*args)
-    jobs = []
+    jobs = Scheduler.jobs_to_perform
 
-    time = Time.now
-
-    Scheduler.enabled.each do |scheduler|
-      # TODO job date start/end check
-      ic_schedule = IceCube::Schedule.new
-      ic_schedule.add_recurrence_rule(IceCube::Rule.from_hash(scheduler.schedule))
-      (jobs << scheduler.job) if scheduler.job.enabled? && ic_schedule.occurrences(time).first.change(hour: scheduler.time.hour, min: scheduler.time.min).between?(time, time + 10.minutes)
-    end
-    jobs.uniq.each do |job|
+    jobs.each do |job|
       JobPerformerJob.perform_later(job)
     end
   end
--- config/initializers/cron_scheduler.rb
+++ config/initializers/cron_scheduler.rb
@@ -1,1 +1,1 @@
-Sidekiq::Cron::Job.create(name: 'Scheduler - every 10min', cron: '*/10 * * * *', class: 'SchedulerWorker') unless Rails.env.test? || Rails.env.development?
+Sidekiq::Cron::Job.create(name: 'Scheduler - every 10min', cron: '*/1 * * * *', class: 'SchedulerWorker') unless Rails.env.test? || Rails.env.development?
